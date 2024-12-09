8f4429ca4c31a7ee893001106cd02a3cbac1492c
Ramazan Carpan <ramazan.carpan@outlook.com>
2024-12-09 21:56:45 +1100

    update footer section

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,41 +1,62 @@
 import React from 'react';
 
-
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>
-      <a href="mailto:ramazan.carpan@outlook.com" className="footer__link">
-        ramazan.carpan@outlook.com
-      </a>
-      </div>
-      <div>
-      <a href="tel:+610431561355" className="footer__link">
-        +61 0431561355
-      </a>
+      <div className="footer__contact">
+        <a href="mailto:ramazan.carpan@outlook.com" className="footer__link">
+          ramazan.carpan@outlook.com
+        </a>
+        <span> | </span>
+        <a href="tel:+610431561355" className="footer__link">
+          +61 0431561355
+        </a>
       </div>
       <ul className="social-list">
         <li className="social-list__item">
-          <a className="social-list__link" href="https://linkedin.com">
+          <a
+            className="social-list__link"
+            href="https://linkedin.com/in/ramazan-carpan-38797b2a5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-linkedin"></i>
           </a>
         </li>
         <li className="social-list__item">
-          <a className="social-list__link" href="https://github.com">
+          <a
+            className="social-list__link"
+            href="https://github.com/ramazan-carpan"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-github"></i>
           </a>
         </li>
-        <li class="social-list__item">
-            <a class="social-list__link" href="http://instagram.com">
-                <i class="fa-brands fa-instagram"></i>
-            </a>
+        <li className="social-list__item">
+          <a
+            className="social-list__link"
+            href="http://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
         </li>
-        <li class="social-list__item">
-            <a class="social-list__link" href="https://twitter.com">
-                <i class="fab fa-twitter"></i>
-            </a>
+        <li className="social-list__item">
+          <a
+            className="social-list__link"
+            href="https://twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
         </li>
       </ul>
+      <p className="footer__copyright">
+        &copy; {new Date().getFullYear()} Ramazan Carpan. All rights reserved.
+      </p>
     </footer>
   );
 };
